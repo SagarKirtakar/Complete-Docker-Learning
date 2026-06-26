7d7617b57d8d04baac2169da629eef5ab9f1db21
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-26 21:30:09 +0530

    rest-demo project for docker 1.0

---
+++ RestDemo/src/main/java/com/sagar/RestDemo/HomeController.java
@@ -0,0 +1,13 @@
+package com.sagar.RestDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+}
