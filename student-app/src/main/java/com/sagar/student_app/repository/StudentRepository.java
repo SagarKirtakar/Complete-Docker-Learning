1a47e3081c0709df30aaa3ea64d26a5ff3c59429
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-07-01 09:20:13 +0530

    docker compose project working

--- student-app/src/main/java/com/sagar/student_app/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sagar.student_app.repository;
-
-import com.sagar.student_app.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
